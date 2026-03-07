620edf0ffef158b80da1e88f0342d60d7cb5c44d
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-07 15:55:29 +1100

    Terraform: Redis SG rules added

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -18,6 +18,16 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   security_group_id = local.mongodb_sg_id
 }
 
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  // which means redis accepting bastion sg id's
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.redis_sg_id
+}
+
 resource "aws_security_group_rule" "mongodb_catalogue" {
   type              = "ingress"
   from_port         = 27017
