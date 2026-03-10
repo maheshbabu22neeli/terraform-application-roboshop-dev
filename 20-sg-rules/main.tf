8bb29c102fe136145bc7fe4d3280401eb52dd2dd
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-10 21:27:44 +1100

    Terraform: Application Load Balancer configuration added

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -50,6 +50,15 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
   security_group_id = local.rabbitmq_sg_id
 }
 
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  // which means backend_alb accepting bastion sg id's
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
 
 resource "aws_security_group_rule" "mongodb_catalogue" {
   type              = "ingress"
