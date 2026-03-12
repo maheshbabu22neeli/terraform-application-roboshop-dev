a6ff989c173a6b5a75e66285d1f2cdac32d531d4
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-12 22:26:40 +1100

    Terraform: remote catalogue route53 entry

--- 60-catalogue/route53.tf
+++ 60-catalogue/route53.tf
@@ -1,8 +1,10 @@
-resource "aws_route53_record" "catalogue" {
+#this is no more required as we are calling the service using ALB
+
+/*resource "aws_route53_record" "catalogue" {
   zone_id         = var.zone_id
   name            = "rabbitmq-${var.environment}.${var.domain_name}"
   type            = "A"
   ttl             = 1
   records         = [aws_instance.catalogue.private_ip]
   allow_overwrite = true
-}
+}*/
