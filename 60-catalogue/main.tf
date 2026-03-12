eea28e74fb23b51c007953daf9c4b1b200e9fa0e
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-12 22:00:36 +1100

    Terraform: LaunchTemplate, Autoscaling, TargetGroup configuration to create autoscale for EC2 instances

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -42,13 +42,13 @@ resource "terraform_data" "catalogue_remote" {
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state       = "stopped"
-  depends_on = [terraform_data.catalogue_remote]
+  depends_on  = [terraform_data.catalogue_remote]
 }
 
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id
-  depends_on = [aws_ec2_instance_state.catalogue]
+  depends_on         = [aws_ec2_instance_state.catalogue]
 
   tags = merge(
     local.common_tags,
@@ -56,4 +56,163 @@ resource "aws_ami_from_instance" "catalogue" {
       Name = "${var.project}-${var.environment}-catalogue"
     }
   )
+}
+
+resource "aws_lb_target_group" "catalogue" {
+  name                 = "${var.project}-${var.environment}-catalogue"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 60
+
+  health_check {
+    enabled = true
+
+    protocol = "HTTP"
+    path     = "/health"
+    port     = 8080
+    matcher  = "200-299"
+
+    healthy_threshold   = 2
+    interval            = 10
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+
+  # this is the AMI created from EC2 instance
+  image_id = aws_ami_from_instance.catalogue.id
+  # We completely want to shutdown/terminate instead of stopping
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = "t2.micro"
+  vpc_security_group_ids               = [local.catalogue_sg_id]
+
+  # each time we apply terraform this version will be updated as default
+  # Which means everytime a new launch template will be created and mark it as default
+  update_default_version = true
+
+  // tags for instances created by launch template
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      }
+    )
+  }
+
+  // tags for volumes created by instances
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      }
+    )
+  }
+
+  // launch template tags
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-catalogue"
+    }
+  )
+}
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.project}-${var.environment}-catalogue"
+  max_size                  = 5
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = [local.private_subnet_id]
+  target_group_arns   = [aws_lb_target_group.catalogue.arn]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+
+  # this is a dynamic block
+  dynamic "dynamicTag" {
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      }
+    )
+
+    content {
+      key                 = dynamicTag.key
+      value               = dynamicTag.value
+      propagate_at_launch = true
+    }
+  }
+
+  # with in 15 mins autoscaling must be successful, other wise timeout and delete the resource
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${var.project}-${var.environment}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 60.0
+  }
+}
+
+resource "aws_lb_listener_rule" "host_based_weighted_routing" {
+  listener_arn = local.backend_alb_listener_arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+
+  condition {
+    host_header {
+      values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
+resource "terraform_data" "catalogue_delete" {
+  # Use triggers to rerun this block if the instance IP changes
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  # this command executes in bastion instance
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+  }
+
+  depends_on = [aws_autoscaling_policy.catalogue]
 }
